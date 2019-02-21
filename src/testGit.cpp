f76575c6f51030fb11a05fc5a621f33299e0a83d
cswt <15575202581@163.com>
2019-02-21 08:50:29 +0800

    testGit

---
+++ src/testGit.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : testGit.cpp
+// Author      : Apache-Wang
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
